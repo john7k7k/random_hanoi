87c6682b5bf1a4931af6433eb40322fb98b6ed93
Your Name <you@example.com>
2024-12-04 18:06:27 +0800

    init

---
+++ hanoi.hpp
@@ -0,0 +1,42 @@
+# include <cstdio>
+
+int move(int *, int *);
+int printTower(int *, int);
+int hanoiMove(int, int *, int *, int *);
+
+int hanoiMove(int amount, int *_source, int *_temp, int *_destination){
+    if(amount == 1) {
+        if(move(_source, _destination) < 0) return -1;
+        return 1;
+    }
+    if(hanoiMove(amount - 1, _source, _destination, _temp) < 0) return -1;
+    if(move(_source, _destination) < 1) return -1;
+    if(hanoiMove(amount - 1, _temp, _source, _destination) < 0) return -1;
+    return 1;
+}
+
+int move(int *_source, int *_destination){
+    if(_source[0] == 0) return -1; //來源沒有物品可以搬運
+    for(int i = 0; i < TOWER_HEIGHT; i++){
+        if(i < TOWER_HEIGHT - 1 && _source[i] > 0) continue;
+        if(i == TOWER_HEIGHT - 1  && _source[i] != 0) i = TOWER_HEIGHT;
+        for(int j = 0; j < TOWER_HEIGHT; j++){
+            if(_destination[j] > 0) continue;
+            if(j > 0 && _destination[j - 1] < _source[i - 1]) return -2; //目的地物品無法承受來源物品重量
+            _destination[j] =  _source[i - 1];
+            _source[i - 1] = 0;
+            break;
+        }
+        break;
+    }
+    return 1; //搬運完成
+}
+
+int printTower(int *tower, int size){
+    for(int i = 0; i < size; i++){
+        printf("%d", tower[i]);
+        if(i < size - 1) printf(",");
+    }
+    printf("\n");
+    return 0;
+}
---
+++ random_hanoi.cpp
@@ -0,0 +1,135 @@
+#define TOWER_HEIGHT 4
+#include <cstdio>
+#include <cstdlib>
+#include <random>
+#include "hanoi.hpp"
+#define PRINT_TOWERS() printTower(sources[0], TOWER_HEIGHT);printTower(sources[1], TOWER_HEIGHT);printTower(destination, TOWER_HEIGHT)
+
+int** genarateRandomTwoSource();
+int getSlayBlock(int *, int);
+int randomTwoSourceHanoiMove(int *, int *, int *);
+
+int main(int argc, char *argv[]){
+    int **sources = genarateRandomTwoSource();
+    int destination[TOWER_HEIGHT] = {0};
+    if(argc > 1){
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 4; j++){
+                sources[i][j] = argv[i+1][j] - '0';
+            }
+        }
+    }
+    
+    PRINT_TOWERS();
+    randomTwoSourceHanoiMove(sources[0], sources[1], destination);
+    printf("moved\n");
+    PRINT_TOWERS();
+    free(sources[0]);
+    free(sources[1]);
+    free(sources);
+    return 0;
+}
+
+int** genarateRandomTwoSource(){
+    int **_sources = (int **)calloc(2, sizeof(int *));
+    _sources[0] = (int *)calloc(TOWER_HEIGHT, sizeof(int));
+    _sources[1] = (int *)calloc(TOWER_HEIGHT, sizeof(int));
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0,1);
+    for(int i = 0; i < TOWER_HEIGHT; i++){
+        int random_number = dis(gen);
+        for(int j = 0; j < TOWER_HEIGHT; j++){
+            if(_sources[random_number][j] > 0) continue;
+            _sources[random_number][j] = TOWER_HEIGHT - i;
+            break;
+        }
+    }
+    return _sources;
+}
+
+int getSlayBlock(int *tower, int height){
+    for(int i = 0; i < height; i++){
+        if(tower[i] > 0) continue;
+        if(i == 0) return 0;
+        return tower[i - 1];
+    }
+    return tower[height - 1];
+}
+
+int randomTwoSourceHanoiMove(int *_source1, int *_source2, int *_destination){
+    int slay1 = getSlayBlock(_source1, TOWER_HEIGHT), slay2 = getSlayBlock(_source2, TOWER_HEIGHT);
+    /*狀況一*/
+    if(slay1 == 0) hanoiMove(TOWER_HEIGHT, _source2, _source1, _destination);
+    else if(slay2 == 0) hanoiMove(TOWER_HEIGHT, _source1, _source2, _destination);
+    /*狀況二*/
+    else if(slay1 == 4){
+        move(_source1, _destination);
+        hanoiMove(3, _source2, _source1, _destination);
+    }
+    else if(slay2 == 4){
+        move(_source2, _destination);
+        hanoiMove(3, _source1, _source2, _destination);
+    }
+    /*狀況三跟四*/
+    else if(slay1 == 3){
+        hanoiMove(2, _source2, _destination, _source1);
+        if(getSlayBlock(_source2, TOWER_HEIGHT) == 4){
+            move(_source2, _destination);
+            hanoiMove(3, _source1, _source2, _destination);
+        }
+        else{
+            hanoiMove(4, _source1, _source2, _destination);
+        }
+    }
+    else if(slay2 == 3){
+        hanoiMove(2, _source1, _destination, _source2);
+        if(getSlayBlock(_source1, TOWER_HEIGHT) == 4){
+            move(_source1, _destination);
+            hanoiMove(3, _source2, _source1, _destination);
+        }
+        else{
+            hanoiMove(4, _source2, _source1, _destination);
+        }
+    }
+    /*狀況五六七八*/
+    else if(slay1 == 2){
+        move(_source2, _source1);
+        slay2 = getSlayBlock(_source2, TOWER_HEIGHT);
+        if(slay2 == 0) hanoiMove(4, _source1, _source2, _destination);
+        else if(slay2 == 4){
+            move(_source2, _destination);
+            hanoiMove(3, _source1, _source2,_destination);
+        }
+        else{
+            hanoiMove(2, _source1, _destination, _source2);
+            slay1 = getSlayBlock(_source1, TOWER_HEIGHT);
+            if(slay1 == 4){
+                move(_source1, _destination);
+                hanoiMove(3, _source2, _source1, _destination);
+            }
+            else if(slay1 == 0) hanoiMove(4, _source2, _source1, _destination);
+            else return -1; //錯誤
+        }
+    }
+    else if(slay2 == 2){
+        move(_source1, _source2);
+        slay1 = getSlayBlock(_source1, TOWER_HEIGHT);
+        if(slay1 == 0) hanoiMove(4, _source2, _source1, _destination);
+        else if(slay1 == 4){
+            move(_source1, _destination);
+            hanoiMove(3, _source2, _source1,_destination);
+        }
+        else{
+            hanoiMove(2, _source2, _destination, _source1);
+            slay2 = getSlayBlock(_source2, TOWER_HEIGHT);
+            if(slay2 == 4){
+                move(_source2, _destination);
+                hanoiMove(3, _source1, _source2, _destination);
+            }
+            else if(slay2 == 0) hanoiMove(4, _source1, _source2, _destination);
+            else return -1; //錯誤
+        }
+    }
+    return 0;
+}
